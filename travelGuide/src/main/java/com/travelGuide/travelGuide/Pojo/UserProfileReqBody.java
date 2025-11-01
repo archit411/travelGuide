3c14ed0bd1ffe9bba0e1dd0af859888e43fe89d4
archit411 <archit@gmail.com>
2025-11-02 02:30:52 +0530

    profile req body comitted

---
+++ travelGuide/src/main/java/com/travelGuide/travelGuide/Pojo/UserProfileReqBody.java
@@ -0,0 +1,95 @@
+package com.travelGuide.travelGuide.Pojo;
+
+public class UserProfileReqBody {
+
+	private String msisdn;
+
+	private String emailId;
+
+	private String fName;
+
+	private String lName;
+
+	private String username;
+
+	private String gender;
+
+	private String city;
+
+	private String state;
+
+	private String country;
+
+	public String getMsisdn() {
+		return msisdn;
+	}
+
+	public void setMsisdn(String msisdn) {
+		this.msisdn = msisdn;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
