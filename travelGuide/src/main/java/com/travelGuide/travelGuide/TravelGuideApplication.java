e00ff08637e9d18a6c4ac27f9e0bfaad6047f05a
archit411 <archit@gmail.com>
2025-11-11 21:01:54 +0530

    travelGuilde main class

--- travelGuide/src/main/java/com/travelGuide/travelGuide/TravelGuideApplication.java
+++ travelGuide/src/main/java/com/travelGuide/travelGuide/TravelGuideApplication.java
@@ -10,15 +10,17 @@ public class TravelGuideApplication {
 
 	public static void main(String[] args) {
 		
-		/*
-		 * //Dotenv dotenv = Dotenv.load(); Dotenv dotenv = Dotenv.configure()
-		 * .ignoreIfMissing() .load();
-		 * 
-		 * 
-		 * 
-		 * // Set env variables so Spring can read them dotenv.entries().forEach(entry
-		 * -> System.setProperty(entry.getKey(), entry.getValue()) );
-		 */
+		
+		  //Dotenv dotenv = Dotenv.load(); 
+		  Dotenv dotenv = Dotenv.configure()
+		  .ignoreIfMissing() .load();
+		  
+		  
+		  
+		  // Set env variables so Spring can read them 
+		  dotenv.entries().forEach(entry
+		  -> System.setProperty(entry.getKey(), entry.getValue()) );
+		 
         
 		SpringApplication.run(TravelGuideApplication.class, args);
 	}
